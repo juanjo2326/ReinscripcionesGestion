f0fc69dd0deb2f2fccad044d1409035f6e1e2495
Jesus-devops <jesus-c-97@outlook.com>
2020-05-08 05:14:54 -0500

    ssss

--- front/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$con = $mysql= new mysqli("localhost", "root","","reinscripciones");
-if ($mysql->connect_error) {
-	die("problemas con la conexion a la base de datos");
-}
-mysqli_set_charset($con, 'utf8');
- ?>
